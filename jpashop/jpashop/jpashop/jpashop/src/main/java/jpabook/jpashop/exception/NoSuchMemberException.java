b80615ce69aea3426cbd4a7b90d43cd40af5bb66
김선규 <106080105+sungyu1@users.noreply.github.com>
2025-08-14 17:39:06 +0900

    DB수정 및 전체 로직 수정
    DB수정 및 전체 로직 수정

--- jpashop/jpashop/jpashop/jpashop/src/main/java/jpabook/jpashop/exception/NoSuchMemberException.java
+++
@@ -1,7 +0,0 @@
-package jpabook.jpashop.exception;
-
-public class NoSuchMemberException extends RuntimeException{
-    public  NoSuchMemberException(){
-        super(("존재하지 않는 아이디 입니다."));
-    }
-}
--- jpashop/jpashop/jpashop/jpashop/src/main/resources/static/js/login-form.js
+++
@@ -1,8 +0,0 @@
-// /static/js/login-form.js
-window.addEventListener('DOMContentLoaded', () => {
-    const signupModal = document.getElementById('modal-signup');
-    if (signupModal) new bootstrap.Modal(signupModal).show();
-
-    const badModal = document.getElementById('modal-bad');
-    if (badModal) new bootstrap.Modal(badModal).show();
-});
